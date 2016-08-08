328bfc58560db69a52d4b7b03acdaad6db46b94e
priyavpd <vpriyacse2@gmail.com>
2016-08-08 03:20:13 -0700

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,22 @@
+# priyakart
+public class Sample
+{
+public sytatic void main(String[] args)
+{
+protected boolean isSorted(boolean ascending) {
+    boolean result = false; 
+
+    if (ascending) { 
+        for (int i=0;i<data.length-1;i++) {
+            if(data[i] < data[i+1]) {
+                result = true;
+            } else if(data[i] > data[i+1]) {
+                result = false;
+            }
+        }
+    } else {
+    return false;
+    }
+}
+}
+}
